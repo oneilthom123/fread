24cbe32f4aa4693eba34cd93c2df48ce1c8c4830
oneilthom123 <84682837+oneilthom123@users.noreply.github.com>
2021-05-24 00:55:49 +0530

    Update daemon.py

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -26,7 +26,7 @@ from nuxhash.version import __version__
 
 BENCHMARK_SECS = 60
 DONATE_PROB = 0.005
-DONATE_ADDRESS = '3GR2QAZTY76DXNTZwi3dUVVZ8LLpkmBy5f'
+DONATE_ADDRESS = '3Q7jfE93RkdgVw3qSeEwHGd9qRZp2WrMxr'
 
 
 def main():
